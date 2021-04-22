69733e98c8506d0dbeb975253851705da345e6b2
zld <ldzhang@aceinna.com>
2021-04-22 16:00:48 +0800

    20210422 updated

--- SPI_MasterV1.0.4/software/Trunk/include/ConfigureGPIOPins.h
+++
@@ -1,7 +0,0 @@
-// Configure GPIO pins
-void InitGPIOPin_DataReady( void );
-void InitGPIOPin_Sync( void );
-void InitGPIOPin_Ex( void );
-void InitGPIOPin_SharedCommunication( void );
-
-//void InitGPIOPin_A4( void );
--- SPI_MasterV1.0.4/software/tags/v0.1.7_ical/include/utilities.h
+++
@@ -1,3 +0,0 @@
-extern char* strtok_r(char *str, char const token, char **cursor);
-extern void strrep(char *str, const char find, const char rep);
-extern int strcmpi(char const *a, char const *b);
